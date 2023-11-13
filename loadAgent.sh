754436c52652571e43587eb2d52ec917ac2fcd59
zred <zachary.redmond@gmail.com>
2023-11-13 09:08:42 -0600

    readability

--- loadAgent.sh
+++ loadAgent.sh
@@ -2,17 +2,18 @@
 
 agentEnv=~/.ssh/.env
 
-if [[ -f $agentEnv ]]; then
-    . $agentEnv > /dev/null
-    if [[ -S $SSH_AUTH_SOCK ]]; then
-        if [[ -n $SSH_AGENT_PID ]] && [[ -f /proc/$SSH_AGENT_PID/cmdline ]] && grep -q ssh-agent /proc/$SSH_AGENT_PID/cmdline; then
-		exit 0
-        else
-            eval $(ssh-agent | tee $agentEnv)
-        fi
-    else
-        eval $(ssh-agent | tee $agentEnv)
-    fi
-else
+# Function to start SSH agent
+start_ssh_agent() {
     eval $(ssh-agent | tee $agentEnv)
+}
+
+# Function to check if SSH agent is running and valid
+is_valid_ssh_agent() {
+    [[ -S $SSH_AUTH_SOCK ]] && [[ -n $SSH_AGENT_PID ]] && [[ -f /proc/$SSH_AGENT_PID/cmdline ]] && grep -q ssh-agent /proc/$SSH_AGENT_PID/cmdline
+}
+
+# Main script execution
+if [[ ! -f $agentEnv ]] || ! . $agentEnv > /dev/null || ! is_valid_ssh_agent; then
+    start_ssh_agent
 fi
+
